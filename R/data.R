0a973592d07333839bc1ad81b8b969bf272322cc
Ксения Лупырь <kseniia@Ksenias-MacBook-Air.local>
2021-09-21 17:22:15 +0300

    new files by roxygen and separate file with data description

---
+++ R/data.R
@@ -0,0 +1,43 @@
+#' TRBV usage probabilities
+#'
+#' Vectors with probabilities of TRBV usage
+#' All probabilities based on OLGA mouse T beta model
+#'
+#' @source \url{https://github.com/statbiophys/OLGA/tree/master/olga/default_models/mouse_T_beta}
+#'
+#' @author Pavel V. Shelyakin
+"OLGA_V"
+
+#' TRBJ usage probabilities
+#'
+#' Vectors with probabilities of TRBJ usage
+#' All probabilities based on OLGA mouse T beta model
+#'
+#' @source \url{https://github.com/statbiophys/OLGA/tree/master/olga/default_models/mouse_T_beta}
+#'
+#' @author Pavel V. Shelyakin
+"OLGA_J"
+
+#' VJ combinations probabilities
+#'
+#' A data frame with probabilities of VJ combinations
+#' All probabilities based on OLGA mouse T beta model
+#'
+#' @source \url{https://github.com/statbiophys/OLGA/tree/master/olga/default_models/mouse_T_beta}
+#'
+#' @author Pavel V. Shelyakin
+"OLGAVJ"
+
+#' All combinations of V and J segments
+#'
+#' @description
+#' A dataset containing all combinations of V and V segments with V segments'
+#' names in the first column and J segments' names in the second column.
+#'
+#' @format A data frame with 253 rows and 2 variables
+#' \itemize{
+#' \item V TRBV segment name
+#' \item J TRBJ segment name
+#' }
+#' @author Pavel V. Shelyakin
+"VDJT"
