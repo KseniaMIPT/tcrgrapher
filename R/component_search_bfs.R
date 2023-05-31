e1ee75afc4394583f85a2c027b73f4cf41274fd8
KseniaMIPT <ksenia22.11@yandex.ru>
2023-05-31 14:20:02 +0300

    documentation for bfs

--- R/component_search_bfs.R
+++ R/component_search_bfs.R
@@ -2,9 +2,18 @@
 #' @importFrom utils txtProgressBar
 #' @importFrom utils setTxtProgressBar
 NULL
-
-#' @param clonoset clonoset
+#' bfs_for_TCRs
+#'
+#' The function searches for all neighbors of the source (src) clonotype. A neighbor
+#' is a clonotype that differs by one amino acid mismatch.
+#'
+#' @param clonoset To get clonoset use clonoset(your_TCR_grapher_object)
 #' @param src clone_id of the source clonotype
+#' @param comp_id cluster_id for the visited vertexes. All other vertexes will have
+#' -1 cluster_id if there are none. If some cluster_ids have already been set,
+#' they will be kept.
+#' @return The function returns the clonoset where all neighrors of the source
+#' clonotype have the given id.
 #' @export
 bfs_for_TCRs <- function(clonoset, src, comp_id, v_gene = TRUE, j_gene = FALSE){
   if(!('cluster_id' %in% colnames(clonoset))){
@@ -38,6 +47,19 @@ bfs_for_TCRs <- function(clonoset, src, comp_id, v_gene = TRUE, j_gene = FALSE){
   clonoset
 }
 
+#' find_TCR_components_by_bfs
+#'
+#' The function searches for connectivity components of the graph, where every node is a
+#' clonotype with a unique clone_id and edges connect clonotypes that differ by one
+#' amino acid mismatch.
+#'
+#' @param clonoset To get clonoset use clonoset(your_TCR_grapher_object)
+#' @param v_gene Boolean value. If 'v_gene' is 'TRUE', only clonotypes with the
+#' same V genes will be part of one connectivity component. Default value is 'TRUE'.
+#' @param j_gene Boolean value. If 'j_gene' is 'TRUE', only clonotypes with the
+#' same J genes will be part of one connectivity component. Default value is 'FALSE'.
+#' @return The function returns the clonoset with the new column "cluster_id", where
+#' every connectivity component has a unique value.
 #' @export
 find_TCR_components_by_bfs <- function(clonoset, v_gene = TRUE, j_gene = FALSE){
   src <- 1
