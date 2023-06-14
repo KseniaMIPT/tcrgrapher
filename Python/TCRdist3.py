bc2200b23ca7274e2e554e509277b78afaac39cd
KseniaMIPT <ksenia22.11@yandex.ru>
2023-06-14 14:10:33 +0300

    TCRdist3 options

--- Python/TCRdist3.py
+++ Python/TCRdist3.py
@@ -4,31 +4,39 @@
 from tcrdist.repertoire import TCRrep
 from tcrdist.centers import calc_radii
 from tcrdist.background import get_stratified_gene_usage_frequency
-from tcrdist.sample import _default_tcrsampler_olga_mouse_beta
+from tcrdist.sample import _default_sampler_olga
 import pandas as pd
 import os
 
-def tcrdist_radii(df, cores):
-  ts = _default_tcrsampler_olga_mouse_beta()
+def tcrdist_radii(df, cores, organism, chain):
+  
+  ts = _default_sampler_olga(organism = organism, chain = chain)
   ts = get_stratified_gene_usage_frequency(ts = ts, replace = True)
     
   df = pd.read_csv(input_path, sep='\t')
-  df['v_b_gene'] = df['bestVGene'] + '*01'
-  df['j_b_gene'] = df['bestJGene'] + '*01'
-  df['cdr3_b_aa'] = df['cdr3aa']
+  if chain == 'beta':
+    df['v_b_gene'] = df['bestVGene'] + '*01'
+    df['j_b_gene'] = df['bestJGene'] + '*01'
+    df['cdr3_b_aa'] = df['cdr3aa']
+    cell_df = df[['count', 'freq', 'cdr3_b_aa', 'v_b_gene', 'j_b_gene']]
+  elif chain == 'alpha':
+    df['v_a_gene'] = df['bestVGene'] + '*01'
+    df['j_a_gene'] = df['bestJGene'] + '*01'
+    df['cdr3_a_aa'] = df['cdr3aa']
+    cell_df = df[['count', 'freq', 'cdr3_a_aa', 'v_a_gene', 'j_a_gene']]
     
   tr = TCRrep(
-    cell_df = df[['count', 'freq', 'cdr3_b_aa', 'v_b_gene', 'j_b_gene']], 
-    chains = ['beta'],
-    organism = 'mouse',
+    cell_df = cell_df,
+    chains = [chain],
+    organism = organism,
     compute_distances=True)
 
-  df_vj_background = tr.synthesize_vj_matched_background(ts = ts, chain = 'beta')
+  df_vj_background = tr.synthesize_vj_matched_background(ts = ts, chain = chain)
 
   trb = TCRrep(
     cell_df = df_vj_background.copy(),
-    organism = 'mouse', 
-    chains = ['beta'], 
+    organism = organism, 
+    chains = [chain], 
     db_file = 'alphabeta_gammadelta_db.tsv',
     compute_distances = False
     )
@@ -37,7 +45,7 @@ def tcrdist_radii(df, cores):
   radii, thresholds, ecdfs = calc_radii(
     tr = tr, 
     tr_bkgd = trb,
-    chain = 'beta',
+    chain = chain,
     ctrl_bkgd = 10**-5
     )
   tr.clone_df['radius'] = radii
